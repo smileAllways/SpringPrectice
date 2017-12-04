f920203920092626d978d5e9e7d49af03e7e2de7
smileAllways <onara9112@gmail.com>
2017-12-04 17:39:28 +0900

    jdbcTempate and transaction

--- BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -12,7 +12,8 @@ import com.springbook.biz.board.BoardVO;
 @Service("boardService")
 public class BoardServiceImpl implements BoardService {
 	@Autowired
-	private BoardDAO boardDAO;
+	private BoardDAOSpring boardDAO;
+	//private BoardDAO boardDAO;
 
 	@Override
 	public void insertBoard(BoardVO vo) {
